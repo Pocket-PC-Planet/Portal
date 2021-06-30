4a3c613bdf87b76513d160695547de93b27f319e
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2021-06-30 19:17:51 -0400

    Add files via upload

--- Portal/index.php
+++ Portal/index.php
@@ -15,48 +15,47 @@ include('head.html');
 $searchTxt = isset($_GET['q']) ? $_GET['q'] : '';
 
 if ($searchTxt != '') {
-$search_string = "SELECT * FROM `posts` WHERE";
-$display_words = "";
+  $search_string = "SELECT * FROM `posts` WHERE";
+  $display_words = "";
 
-//format the keywords for the query
-$keywords = explode(',', $searchTxt);
-foreach ($keywords as $word) {
-  $search_string .= "`name` LIKE '%".$word."%' OR `content` LIKE '%".$word."%' OR `category` LIKE '%".$word."%' OR `type` LIKE '%".$word."%' OR ";
-  $display_words .= $word.' ';
-}
+  //format the keywords for the query
+  $keywords = explode(',', $searchTxt);
+  foreach ($keywords as $word) {
+    $search_string .= "`name` LIKE '%" . $word . "%' OR `content` LIKE '%" . $word . "%' OR `category` LIKE '%" . $word . "%' OR `type` LIKE '%" . $word . "%' OR ";
+    $display_words .= $word . ' ';
+  }
 
-$search_string = substr($search_string, 0, strlen($search_string)-4);
-$display_words = substr($display_words, 0, strlen($display_words)-1);
+  $search_string = substr($search_string, 0, strlen($search_string) - 4);
+  $display_words = substr($display_words, 0, strlen($display_words) - 1);
 
-//connect to db
-try {
+  //connect to db
+  try {
     $conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-    
+
     $query = mysqli_query($conn, $search_string);
     $result_count = mysqli_num_rows($query);
-    
+
     //display keywords
-    echo '<div class="right"><b><u>'.number_format($result_count).'</u></b> result(s) found in the PPC Planet archive</div>';
-    echo 'Your search for <i>"'.$display_words.'"</i><hr />';
-    
+    echo '<div class="right"><b><u>' . number_format($result_count) . '</u></b> result(s) found in the PPC Planet archive</div>';
+    echo 'Your search for <i>"' . $display_words . '"</i><hr />';
+
     if ($result_count > 0) {
-      //display header for the table
-      //echo '<table class="search>';
+      //if items are found in the PPC Planet archive...
       echo "<h3>PPC Planet Archive Results:</h3>";
       //loop through results
       while ($row = mysqli_fetch_assoc($query)) {
-          echo "<div class='result'><h2><a href='https://archive.ppcplanet.org/?q=".$row["name"]."' target='_blank'>".$row["name"]."</a></h2><p>".$row["content"]."</p><p>Category:&nbsp;<i>".$row['category']."</i></p><hr></div>";
+        //echo name, description, and category. link to archive listing.
+        echo "<div class='result'><h2><a href='https://archive.ppcplanet.org/?q=" . $row["name"] . "' target='_blank'>" . $row["name"] . "</a></h2><p>" . $row["content"] . "</p><p>Category:&nbsp;<i>" . $row['category'] . "</i></p><hr></div>";
       }
     }
-    
-    //list other sources as well...
+
+    //if query isn't blank, list other sources as well...
     if ($searchTxt != "") {
-      echo "<h3>Other Sources:</h3>";
-      echo "<div class='result'><h2><a href='https://archive.org/search.php?query=".$searchTxt."' target='_blank'>Search the Internet Archive for '".$searchTxt."'</a></h2><p>The Internet Archive is a great place to find old software for Pocket PCs. In fact, PPC Planet actually uses it to host the files in its archive!</p></div><hr><div class='result'><h2><a href='https://web.archive.org/web/*/".$searchTxt."' target='_blank'>Search the WayBack Machine for '".$searchTxt."'</a></h2><p>This is the website portion of the Internet Archive. If you are looking for an old file or site, you might be able to find it here.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:oldhandhelds.com ".$searchTxt."' target='_blank'>Search OldHandhelds for '".$searchTxt."'</a></h2><p>OldHandhelds hosts tons of software for Pocket PCs and similar devices.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:mariomasta64.me ".$searchTxt."' target='_blank'>Search MarioMasta64's archive for '".$searchTxt."'</a></h2><p>Another place that hosts Pocket PC software.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=\"".$searchTxt."\"' target='_blank'>Search Google for '".$searchTxt."'</a></h2><p>If all else has failed, perhaps Google can help you find the software you are looking for.</p></div><hr><br><p style='text-align: center'>&copy; PPC Planet Team 2021</p><br>";
+      echo "<h3>Other Sources:</h3><div class='result'><h2><a href='https://archive.org/search.php?query=" . $searchTxt . "' target='_blank'>Search the Internet Archive for '" . $searchTxt . "'</a></h2><p>The Internet Archive is a great place to find old software for Pocket PCs. In fact, PPC Planet actually uses it to host the files in its archive!</p></div><hr><div class='result'><h2><a href='https://web.archive.org/web/*/" . $searchTxt . "' target='_blank'>Search the WayBack Machine for '" . $searchTxt . "'</a></h2><p>This is the website portion of the Internet Archive. If you are looking for an old file or site, you might be able to find it here.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:oldhandhelds.com " . $searchTxt . "' target='_blank'>Search OldHandhelds for '" . $searchTxt . "'</a></h2><p>OldHandhelds hosts tons of software for Pocket PCs and similar devices.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:mariomasta64.me " . $searchTxt . "' target='_blank'>Search MarioMasta64's archive for '" . $searchTxt . "'</a></h2><p>Another place that hosts Pocket PC software.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=\"" . $searchTxt . "\"' target='_blank'>Search Google for '" . $searchTxt . "'</a></h2><p>If all else has failed, perhaps Google can help you find the software you are looking for.</p></div><hr><br><p style='text-align: center'>&copy; PPC Planet Team 2021</p><br>";
     }
-} catch (PDOException $exception) {
-    // If there is an error with the connection, stop the script and display the error
+  } catch (PDOException $exception) {
+    //if there is an error with the connection, stop the script and display the error
     exit('Failed to connect to database!');
-}
+  }
 }
 ?>
