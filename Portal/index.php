aa261949b9eea90fa1f2e1a10a50e524fd6188ea
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2021-07-03 18:58:10 -0400

    Add files via upload

--- Portal/index.php
+++ Portal/index.php
@@ -37,7 +37,7 @@ if ($searchTxt != '') {
 
     //display keywords
     echo '<div class="right"><b><u>' . number_format($result_count) . '</u></b> result(s) found in the PPC Planet archive</div>';
-    echo 'Your search for <i>"' . $display_words . '"</i><hr />';
+    echo 'Your search for <i>"' . $display_words . '"</i>';
 
     if ($result_count > 0) {
       //if items are found in the PPC Planet archive...
@@ -45,13 +45,13 @@ if ($searchTxt != '') {
       //loop through results
       while ($row = mysqli_fetch_assoc($query)) {
         //echo name, description, and category. link to archive listing.
-        echo "<div class='result'><h2><a href='https://archive.ppcplanet.org/?q=" . $row["name"] . "' target='_blank'>" . $row["name"] . "</a></h2><p>" . $row["content"] . "</p><p>Category:&nbsp;<i>" . $row['category'] . "</i></p><hr></div>";
+        echo "<hr><div class='result'><h2><a href='https://archive.ppcplanet.org/?q=" . $row["name"] . "' target='_blank'>" . $row["name"] . "</a></h2><p>" . $row["content"] . "</p><p>Category:&nbsp;<i>" . $row['category'] . "</i></p></div>";
       }
     }
 
     //if query isn't blank, list other sources as well...
     if ($searchTxt != "") {
-      echo "<h3>Other Sources:</h3><div class='result'><h2><a href='https://archive.org/search.php?query=" . $searchTxt . "' target='_blank'>Search the Internet Archive for '" . $searchTxt . "'</a></h2><p>The Internet Archive is a great place to find old software for Pocket PCs. In fact, PPC Planet actually uses it to host the files in its archive!</p></div><hr><div class='result'><h2><a href='https://web.archive.org/web/*/" . $searchTxt . "' target='_blank'>Search the WayBack Machine for '" . $searchTxt . "'</a></h2><p>This is the website portion of the Internet Archive. If you are looking for an old file or site, you might be able to find it here.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:oldhandhelds.com " . $searchTxt . "' target='_blank'>Search OldHandhelds for '" . $searchTxt . "'</a></h2><p>OldHandhelds hosts tons of software for Pocket PCs and similar devices.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:mariomasta64.me " . $searchTxt . "' target='_blank'>Search MarioMasta64's archive for '" . $searchTxt . "'</a></h2><p>Another place that hosts Pocket PC software.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=\"" . $searchTxt . "\"' target='_blank'>Search Google for '" . $searchTxt . "'</a></h2><p>If all else has failed, perhaps Google can help you find the software you are looking for.</p></div><hr><br><p style='text-align: center'>&copy; PPC Planet Team 2021</p><br>";
+      echo "<h3>Other Sources:</h3><hr><div class='result'><h2><a href='https://archive.org/search.php?query=" . $searchTxt . "' target='_blank'>Search the Internet Archive for '" . $searchTxt . "'</a></h2><p>The Internet Archive is a great place to find old software for Pocket PCs. In fact, PPC Planet actually uses it to host the files in its archive!</p></div><hr><div class='result'><h2><a href='https://web.archive.org/web/*/" . $searchTxt . "' target='_blank'>Search the WayBack Machine for '" . $searchTxt . "'</a></h2><p>This is the website portion of the Internet Archive. If you are looking for an old file or site, you might be able to find it here.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:oldhandhelds.com " . $searchTxt . "' target='_blank'>Search OldHandhelds for '" . $searchTxt . "'</a></h2><p>OldHandhelds hosts tons of software for Pocket PCs and similar devices.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=site:mariomasta64.me " . $searchTxt . "' target='_blank'>Search MarioMasta64's archive for '" . $searchTxt . "'</a></h2><p>Another place that hosts Pocket PC software.</p></div><hr><div class='result'><h2><a href='https://google.com/search?q=\"" . $searchTxt . "\"' target='_blank'>Search Google for '" . $searchTxt . "'</a></h2><p>If all else has failed, perhaps Google can help you find the software you are looking for.</p></div><hr><br><p style='text-align: center'>&copy; PPC Planet Team 2021</p><br>";
     }
   } catch (PDOException $exception) {
     //if there is an error with the connection, stop the script and display the error
